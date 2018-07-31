cd16f8954830b23a976bc1433395bc40b199bbb6
IBM <IBM@DEVOPS_02>
2018-07-31 09:06:17 +0530

    done

--- BDD/src/test/java/StepDefinition/StepDefinition.java
+++ BDD/src/test/java/StepDefinition/StepDefinition.java
@@ -15,7 +15,7 @@ public class StepDefinition {
 
 	@Given("^Login should be available$")
 	public void login_should_be_available() {
-		 System.setProperty("webdriver.gecko.driver","E:\\geckodriver-v0.20.1-win64\\geckodriver.exe");  /*"/usr/local/bin/geckodriver"*/ 
+		 System.setProperty("webdriver.gecko.driver","/usr/local/bin/geckodriver");  //"E:\\geckodriver-v0.20.1-win64\\geckodriver.exe"
 		  driver= new FirefoxDriver();
 		  driver.get("http://ec2-18-222-145-43.us-east-2.compute.amazonaws.com:8090/TestWebapp");
 		  
