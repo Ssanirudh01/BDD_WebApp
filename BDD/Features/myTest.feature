1adaa5432b2627ef73d528c2d569b7802384e141
IBM <IBM@DEVOPS_02>
2018-07-31 08:54:11 +0530

    committing

---
+++ BDD/Features/myTest.feature
@@ -0,0 +1,6 @@
+Feature: To test manipal login application
+@login
+Scenario: To validate login application
+Given Login should be available
+When Validate Login application
+Then Login should be successful
---
+++ BDD/src/test/java/StepDefinition/StepDefinition.java
@@ -0,0 +1,42 @@
+package StepDefinition;
+
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class StepDefinition {
+	WebDriver driver;
+
+	@Given("^Login should be available$")
+	public void login_should_be_available() {
+		 System.setProperty("webdriver.gecko.driver","E:\\geckodriver-v0.20.1-win64\\geckodriver.exe");  /*"/usr/local/bin/geckodriver"*/ 
+		  driver= new FirefoxDriver();
+		  driver.get("http://ec2-18-222-145-43.us-east-2.compute.amazonaws.com:8090/TestWebapp");
+		  
+	    
+	}
+	String breakpoint;
+
+	@When("^Validate Login application$")
+	public void validate_Login_application() {
+		driver.findElement(By.name("uname")).sendKeys("admin");
+		  driver.findElement(By.name("psw")).sendKeys("pass");
+		  driver.findElement(By.xpath("//*[@type='submit']")).click();
+		  System.out.println("Sucessful");
+	    
+	}
+
+	@Then("^Login should be successful$")
+	public void login_should_be_successful() {
+		breakpoint=driver.findElement(By.tagName("h2")).getText();
+		  Assert.assertEquals(breakpoint, "Prolearn");
+	    
+	}
+
+}
